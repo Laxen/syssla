e7c0229f38a3e21d74c4a2ad8a50e35a0949a621
Alexander Ganslandt <alex.ganslandt@gmail.com>
2024-02-18 13:18:02 +0100

    frontend: Add reordering

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -10,7 +10,36 @@ export function App() {
   const [state, setState] = useState(initialData)
 
   const onDragEnd = result => {
-    // TODO
+    const { destination, source, draggableId } = result
+
+    if (!destination)
+      return
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return
+
+    const column = state.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn,
+      }
+    };
+
+    setState(newState)
   }
 
   return (
--- frontend/src/initial-data.js
+++ frontend/src/initial-data.js
@@ -2,12 +2,13 @@ const initialData = {
   tasks: {
     "task-1": { id: "task-1", content: "Take out garbage" },
     "task-2": { id: "task-2", content: "Do the other thing" },
+    "task-3": { id: "task-3", content: "Don't do anything!" },
   },
   columns: {
     "column-1": {
       id: "column-1",
       title: "Todo",
-      taskIds: ["task-1", "task-2"],
+      taskIds: ["task-1", "task-2", "task-3"],
     }
   },
   columnOrder: ["column-1"],
